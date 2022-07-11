3c5828d2dabb0735f8ab5dac554f5929409f3fb9
Tebekin <i.tebekin@yandex.ru>
2022-07-11 14:00:30 +0300

    style: привести код к codestyle. правки после замечаний

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -2,24 +2,25 @@ import java.util.Scanner;
 
 public class DepositCalculator {
 
-    double calculatePercentComplex (double contribution, double year, int depositType) {
+    double calculatePercentComplex(double contribution, double year, int depositType) {
         double profit;
 
         profit = contribution * Math.pow((1 + year / 12), 12 * depositType);
-        return roundValue (profit, 2);
+        return roundValue(profit, 2);
     }
 
-    double calculatePercentSimple (double contribution, double yearRate, int depositTime) {
+    double calculatePercentSimple(double contribution, double yearRate, int depositTime) {
         double profit;
 
         profit = contribution + contribution * yearRate * depositTime;
-        return roundValue (profit, 2);
+        return roundValue(profit, 2);
     }
-    double roundValue (double value, int places) {
+
+    double roundValue(double value, int places) {
         double scale;
 
-        scale = Math.pow (10, places);
-        return Math.round(value*scale) / scale;
+        scale = Math.pow(10, places);
+        return Math.round(value * scale) / scale;
     }
 
     void calculateProfit() {
@@ -27,7 +28,6 @@ public class DepositCalculator {
         int contributionType;
 
         Scanner inputUser = new Scanner(System.in);
-
         System.out.println("Введите сумму вклада в рублях:");
         int contributionInput = inputUser.nextInt();
         System.out.println("Введите срок вклада в годах:");
@@ -38,7 +38,7 @@ public class DepositCalculator {
         double profit = 0;
 
         if (contributionType == 1)
-            profit = calculatePercentSimple (contributionInput, 0.06, contributionTime);
+            profit = calculatePercentSimple(contributionInput, 0.06, contributionTime);
         else if (contributionType == 2) {
             profit = calculatePercentComplex(contributionInput, 0.06, contributionTime);
         }
@@ -46,7 +46,7 @@ public class DepositCalculator {
                 + contributionInput + " за " + contributionTime + " лет превратятся в " + profit);
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         new DepositCalculator().calculateProfit();
     }
 }
